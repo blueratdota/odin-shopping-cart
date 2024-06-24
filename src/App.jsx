3cdb9f2e62e97c01e7d300702d701d7151903726
blueratdota <jrafaelgocong@gmail.com>
2024-06-25 04:08:54 +0800

    non overlapping drawer

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { Link, Outlet } from "react-router-dom";
 import { productData } from "./assets/SampleData";
 //import - components
 import ScrollNav from "./components/ScrollNav";
+import HomeCarousel from "./components/HomeCarousel";
 
 function App() {
   return (
     <div className="bg-gray-700">
       <ScrollNav></ScrollNav>
+
       <Outlet></Outlet>
 
       {productData.map((data) => {
--- src/components/FirstTeaser.jsx
+++ src/components/FirstTeaser.jsx
@@ -1,21 +1,29 @@
 const FirstTeaser = () => {
   return (
-    <div className="w-full h-[600px] px-16 gap-10 bg-white flex items-center">
-      <div className="max-w-[50%] grow py-32 text-right">
-        <h2 className="text-2xl font-bold mb-10">
+    <div className="bg-white w-full pt-20 pb-10 text-center md:flex md:gap-20 md:items-center">
+      <div className="md:max-w-[50%] md:text-end md:grow">
+        <h2 className="text-3xl font-medium mb-10 teaser-line relative">
           REALISTIC SIMULATION HARDWARE
         </h2>
-        <div className="flex justify-end">
-          <p className="max-w-[500px]">
+        <div className="flex justify-center mx-auto grow md:hidden">
+          <img
+            src="https://fanatec.com/media/image/64/81/70/DD2_frontpage3.jpg"
+            alt=""
+            className="max-w-[320px]"
+          />
+        </div>
+        <div className="flex justify-center md:justify-end mt-5 px-4 md:px-0 md:pl-5">
+          <p className="max-w-[500px] font-thin text-lg">
             Enthusiast-grade equipment, built by sim racers, for sim racers.
             Designed and engineered in Germany, Fanatec racing simulation
             hardware is built to the highest standards so that you can perform
             at your best. It’s time to stop playing with toys.
           </p>
         </div>
-        <div>skewed button</div>
+        <div className="mt-5">skewed button</div>
       </div>
-      <div className="max-w-[50%] grow">
+
+      <div className="max-w-[50%] grow hidden md:block">
         <img
           src="https://fanatec.com/media/image/64/81/70/DD2_frontpage3.jpg"
           alt=""
@@ -23,6 +31,37 @@ const FirstTeaser = () => {
         />
       </div>
     </div>
+
+    // <div className="w-full h-[600px] px-16 gap-20 bg-white flex items-center">
+    //   <div className="max-w-[50%] grow py-32 text-right">
+    //     <h2 className="text-3xl font-medium mb-10">
+    //       REALISTIC SIMULATION HARDWARE
+    //     </h2>
+    //     <div className="max-w-[50%] grow md:hidden">
+    //       <img
+    //         src="https://fanatec.com/media/image/64/81/70/DD2_frontpage3.jpg"
+    //         alt=""
+    //         className="max-h-[400px]"
+    //       />
+    //     </div>
+    //     <div className="flex justify-end">
+    //       <p className="max-w-[500px] font-thin text-lg">
+    //         Enthusiast-grade equipment, built by sim racers, for sim racers.
+    //         Designed and engineered in Germany, Fanatec racing simulation
+    //         hardware is built to the highest standards so that you can perform
+    //         at your best. It’s time to stop playing with toys.
+    //       </p>
+    //     </div>
+    //     <div className="mt-10">skewed button</div>
+    //   </div>
+    //   <div className="max-w-[50%] grow hidden md:block">
+    //     <img
+    //       src="https://fanatec.com/media/image/64/81/70/DD2_frontpage3.jpg"
+    //       alt=""
+    //       className="max-h-[400px]"
+    //     />
+    //   </div>
+    // </div>
   );
 };
 export default FirstTeaser;
