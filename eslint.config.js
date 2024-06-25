ee657e8b0986612d4e8da06fedfee934a6cf98df
blueratdota <jrafaelgocong@gmail.com>
2024-06-26 01:19:38 +0800

    updated dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,11 @@ import pluginJs from "@eslint/js";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 import { fixupConfigRules } from "@eslint/compat";
 
+
 export default [
-  { languageOptions: { globals: globals.browser } },
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
-  {
-    files: ["**/*.jsx"],
-    languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } }
-  },
-  ...fixupConfigRules(pluginReactConfig)
+  ...fixupConfigRules(pluginReactConfig),
 ];
